0bce8fe4351addd97ae6d2c373253b8f6a678a32
Joshua Tate <jmtate@icloud.com>
2017-04-18 21:16:01 -0600

    Added ch01/bronze

---
+++ ch01/bronze.swift
@@ -0,0 +1,5 @@
+// Create a new instance of the String type
+// Set the value of this instance to be equal to your last name
+var lastName = "Tate"
+// Print its value to the console
+print(lastName)
