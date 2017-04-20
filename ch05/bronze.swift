95369f81596a361b09988d7ee3f0ac08f96b7208
Joshua Tate <jmtate@icloud.com>
2017-04-19 21:59:07 -0600

    Added ch05/bronze and ch05/silver

---
+++ ch05/bronze.swift
@@ -0,0 +1,18 @@
+let point = (x: 1, y: 4)
+
+switch point {
+case let q1 where (point.x > 0) && (point.y > 0):
+    print("\(q1) is in quadrant 1") // Executed
+case let q2 where (point.x < 0) && (point.y > 0):
+    print("\(q2) is in quadrant 2")
+case let q3 where (point.x < 0) && (point.y < 0):
+    print("\(q3) is in quadrant 3")
+case let q4 where (point.x > 0) && (point.y < 0):
+    print("\(q4) is in quadrant 4")
+case (_, 0):
+    print("\(point) lies on the x-axis")
+case (0, _):
+    print("\(point) lies on the y-axis")
+default:
+    print("Case not covered")
+}
---
+++ ch05/silver.swift
@@ -0,0 +1,5 @@
+let age = 26
+
+if case 18...35 = age, age >= 21, age < 30 {
+    print("You're cool, you can drink, and your not old!")
+}
