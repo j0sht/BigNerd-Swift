f288fce24e1d94eabbcd08a2ea112445f9c7c8d1
Joshua Tate <jmtate@icloud.com>
2017-04-19 22:02:46 -0600

    changed if statements to if case statements in ch03/bronze

--- ch03/bronze.swift
+++ ch03/bronze.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 let population: Int
 if CommandLine.arguments.count > 1 {
     population = Int(CommandLine.arguments[1]) ?? 5422
@@ -8,13 +6,11 @@ if CommandLine.arguments.count > 1 {
 }
 
 let message: String
-let hasPostOffice = true
-
 if population < 10000 {
     message = "\(population) is a small town!"
-} else if (10000..<50000).contains(population) {
+} else if case 10000..<50000 = population {
     message = "\(population) is a medium town!"
-} else if (50000..<1000000).contains(population) {
+} else if case 50000..<1000000 = population {
     message = "\(population) is pretty big!"
 } else {
     message = "\(population) is yuge!"
