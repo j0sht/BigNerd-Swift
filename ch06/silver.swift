139acc8ec172138b59f2d51a57907d9beac3b484
Joshua Tate <jmtate@icloud.com>
2017-04-20 21:35:55 -0600

    Added ch06/silver with switch

--- ch06/silver.swift
+++ ch06/silver.swift
@@ -1,12 +1,13 @@
 for i in 0...100 {
-    var s = ""
-    if i % 3 == 0 {
-        s += "Fizz"
-    }
-    if i % 5 == 0 {
-        s += "Buzz"
-    }
-    if s == "" {
+    let s: String
+    switch (i % 3, i % 5) {
+    case (0, 0):
+        s = "FizzBuzz"
+    case (0, _):
+        s = "Fizz"
+    case (_, 0):
+        s = "Buzz"
+    default:
         s = String(i)
     }
     print(s, terminator:", ")
