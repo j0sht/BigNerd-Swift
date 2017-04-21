0d300d6703401cbeb400aa87b82be7bf4c5f4185
Joshua Tate <jmtate@icloud.com>
2017-04-21 16:01:36 -0600

    Added ch07/bronze

---
+++ ch07/bronze.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// Create a new String instance called empty, give it an empty string
+let empty = ""
+
+// Use startIndex and endIndex on empty to determine if it is truly empty
+print("empty startIndex: \(empty.startIndex)")
+print("empty endIndex: \(empty.endIndex)")
+
+// Cleaner
+print("empty.isEmpty: \(empty.isEmpty)")
