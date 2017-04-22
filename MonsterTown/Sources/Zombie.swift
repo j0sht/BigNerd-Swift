e512628cfe0831a97d29089c5f76900640d21f32
Joshua Tate <jmtate@icloud.com>
2017-04-22 17:56:05 -0600

    My solution to ch15 bronze challenge

--- MonsterTown/Sources/Zombie.swift
+++ MonsterTown/Sources/Zombie.swift
@@ -4,7 +4,12 @@ class Zombie: Monster {
     var walksWithLimp = true
 
     final override func terrorizeTown() {
-        town?.changePopulation(by: -10)
+        guard let t = town, t.population > 0  else {
+            super.terrorizeTown()
+            return
+        }
+        let pop = t.population
+        town?.changePopulation(by: (pop < 10) ? 0 - pop : -10)
         super.terrorizeTown()
     }
 }
--- MonsterTown/Sources/main.swift
+++ MonsterTown/Sources/main.swift
@@ -8,3 +8,14 @@ let fredTheZombie = Zombie()
 fredTheZombie.town = myTown
 fredTheZombie.terrorizeTown()
 fredTheZombie.town?.printDescription()
+
+// Bronze challenge test
+var yourTown = Town()
+yourTown.population = 7
+yourTown.printDescription()
+let jillTheZombie = Zombie()
+jillTheZombie.town = yourTown
+jillTheZombie.terrorizeTown()
+jillTheZombie.town?.printDescription()
+jillTheZombie.terrorizeTown()
+jillTheZombie.town?.printDescription()
