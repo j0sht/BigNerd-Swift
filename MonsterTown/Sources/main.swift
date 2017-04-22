31d6c2a03c6442dde9b55a595196115a04ecb50f
Joshua Tate <jmtate@icloud.com>
2017-04-22 17:25:09 -0600

    Added Monster class

--- MonsterTown/Sources/main.swift
+++ MonsterTown/Sources/main.swift
@@ -3,3 +3,6 @@ import Foundation
 var myTown = Town()
 myTown.changePopulation(by: 500)
 myTown.printDescription()
+let genericMonster = Monster()
+genericMonster.town = myTown
+genericMonster.terrorizeTown()
