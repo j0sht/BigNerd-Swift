b6e60e112923ae10705941575bb0a9a8190c677b
Joshua Tate <jmtate@icloud.com>
2017-04-22 18:22:46 -0600

    Refactored Zombie

--- MonsterTown/Sources/Zombie.swift
+++ MonsterTown/Sources/Zombie.swift
@@ -4,11 +4,10 @@ class Zombie: Monster {
     var walksWithLimp = true
 
     final override func terrorizeTown() {
-        guard let t = town, t.population > 0  else {
+        guard let pop = town?.population, pop > 0  else {
             super.terrorizeTown()
             return
         }
-        let pop = t.population
         town?.changePopulation(by: (pop < 10) ? 0 - pop : -10)
         super.terrorizeTown()
     }
