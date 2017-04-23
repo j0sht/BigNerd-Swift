e8e78d3640e30fde37d18caceabd1ff1f7669294
Joshua Tate <jmtate@icloud.com>
2017-04-22 18:26:00 -0600

    Refactored guard to if-let in Zombie

--- MonsterTown/Sources/Zombie.swift
+++ MonsterTown/Sources/Zombie.swift
@@ -4,11 +4,9 @@ class Zombie: Monster {
     var walksWithLimp = true
 
     final override func terrorizeTown() {
-        guard let pop = town?.population, pop > 0  else {
-            super.terrorizeTown()
-            return
+        if let pop = town?.population, pop > 0 {
+            town?.changePopulation(by: (pop < 10) ? 0 - pop : -10)
         }
-        town?.changePopulation(by: (pop < 10) ? 0 - pop : -10)
         super.terrorizeTown()
     }
 }
