064a58807ed8889e2f0f52144873d3f28fd40e97
Joshua Tate <jmtate@icloud.com>
2017-04-25 18:46:44 -0600

    Added padding to printTable

--- ch19/format_table.swift
+++ ch19/format_table.swift
@@ -2,10 +2,16 @@ import Foundation
 
 func printTable(_ data: [[String]],
                 withColumnLabels columnLabels: String...) {
+    let columnWidths = columnLabels.map { $0.characters.count }
     let headers = columnLabels.reduce("|") { $0 + " \($1) |" }
     print(headers)
     for row in data {
-        let out = row.reduce("|") { $0 + " \($1) |" }
+        let out = row.enumerated().reduce("|") { (acc, tup) in
+            let (i, item) = tup
+            let paddingNeeded = columnWidths[i] - item.characters.count
+            let padding = String(repeating: " ", count: paddingNeeded)
+            return acc + " \(padding)\(item) |"
+        }
         print(out)
     }
 }
