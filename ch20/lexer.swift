5caa8b7c87d6bce8bf046434bb309714aa52754a
Joshua Tate <jmtate@icloud.com>
2017-04-27 21:47:41 -0600

    Added Lexer

---
+++ ch20/lexer.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+enum Token {
+    case number(Int)
+    case plus
+}
+
+class Lexer {
+    enum Error: Swift.Error {
+        case invalidCharacter(Character)
+    }
+    let input: String.CharacterView
+    var position: String.CharacterView.Index
+
+    init(input: String) {
+        self.input = input.characters
+        self.position = self.input.startIndex
+    }
+
+    func peek() -> Character? {
+        guard position < input.endIndex else { return nil }
+        return input[position]
+    }
+    func advance() {
+        assert(position < input.endIndex, "Cannot advance past endIndex!")
+        position = input.index(after: position)
+    }
+    func lex() throws -> [Token] {
+        var tokens = [Token]()
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0"..."9":
+                // Start of a number - need to grab the rest
+                let value = getNumber()
+                tokens.append(.number(value))
+            case "+":
+                tokens.append(.plus)
+                advance()
+            case " ":
+                // Just advance to ignore spaces
+                advance()
+            default:
+                // Something unexpected - need to send back an error
+                throw Lexer.Error.invalidCharacter(nextCharacter)
+            }
+        }
+        return tokens
+    }
+    func getNumber() -> Int {
+        var value = 0
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0"..."9":
+                // Another digit - add it into the value
+                let digitValue = Int(String(nextCharacter))!
+                value = 10*value + digitValue
+                advance()
+            default:
+                // A nondigit - go back to regular lexing
+                return value
+            }
+        }
+        return value
+    }
+}
+
+func evaluate(_ input: String) {
+    print("Evaluating: \(input)")
+    let lexer = Lexer(input: input)
+    do {
+        let tokens = try lexer.lex()
+        print("Lexer output: \(tokens)")
+    } catch Lexer.Error.invalidCharacter(let c) {
+        print("Input contained an invalid character: \(c)")
+    } catch {
+        print("An error occured: \(error)")
+    }
+}
+
+evaluate("10 + 3 + 5")
