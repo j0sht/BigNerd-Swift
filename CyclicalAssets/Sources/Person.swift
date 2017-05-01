cff5b762904e785757bdcbc15e2db174827fad58
Joshua Tate <jmtate@icloud.com>
2017-05-01 13:14:21 -0600

    Added reference cycles in closures demo

--- CyclicalAssets/Sources/Person.swift
+++ CyclicalAssets/Sources/Person.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class Person: CustomStringConvertible {
     let name: String
+    let accountant = Accountant()
     private(set) var assets = [Asset]()
     // CustomStringConvertible
     var description: String {
@@ -9,12 +10,28 @@ class Person: CustomStringConvertible {
     }
     init(name: String) {
         self.name = name
+        // [weak self] is a capture list
+        // [weak self] tells Swift to capute self weakly
+        // instead of the default strong
+        // Necessary here to prevent a strong-reference cycle between
+        // Person and Accountant via netWorthChangedHandler
+        accountant.netWorthChangedHandler = { [weak self] netWorth in
+            // Because self is captured weakly, and all weak
+            // instances must be optional, self inside the
+            // closure is optional
+            self?.netWorthDidChange(to: netWorth)
+            return
+        }
+    }
+    deinit {
+        print("\(self) is being deallocated")
     }
     func addAsset(_ asset: Asset) {
         asset.owner = self
         assets.append(asset)
+        accountant.gained(asset)
     }
-    deinit {
-        print("\(self) is being deallocated")
+    func netWorthDidChange(to netWorth: Double) {
+        print("The net worth of \(self) is now \(netWorth)")
     }
 }
