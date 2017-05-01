a9f0827135b67b5660a7d31b7c0c35f1a39db65f
Joshua Tate <jmtate@icloud.com>
2017-04-30 22:27:47 -0600

    Finished ch23/protocol_extensions example and ch23/bronze challenge

--- ch23/protocol_extensions.swift
+++ ch23/protocol_extensions.swift
@@ -1,12 +1,26 @@
 import Foundation
 
-protocol Exercise {
+protocol Exercise: CustomStringConvertible {
     var name: String { get }
     var caloriesBurned: Double { get }
     var minutes: Double { get }
+    var title: String { get }
+}
+extension Exercise {
+    var description: String {
+        let cals = caloriesBurned
+        let mins = minutes
+        return "Exercise \(name): burned \(cals) calories in \(mins) minutes"
+    }
+}
+extension Exercise {
+    var title: String {
+        return "\(name) - \(minutes) minutes"
+    }
 }
 struct EllipticalWorkout: Exercise {
     let name = "Elliptical Workout"
+    let title = "Workout using the Go Fast Elliptical Trainer 3000"
     let caloriesBurned: Double
     let minutes: Double
 }
@@ -18,16 +32,28 @@ struct TreadmillWorkout: Exercise {
     let minutes: Double
     let laps: Double
 }
+extension TreadmillWorkout {
+    var description: String {
+        let c = caloriesBurned
+        let m = minutes
+        let l = laps
+        return "TreadMillWorkout: \(c) calories, \(l) laps in \(m) minutes"
+    }
+}
 let runningWorkout = TreadmillWorkout(caloriesBurned: 350,
                                       minutes: 25,
                                       laps: 10.5)
 extension Exercise {
     var caloriesBurnedPerMinute: Double { return caloriesBurned / minutes }
 }
+// Output
 let workouts: [Exercise] = [ellipticalWorkout, runningWorkout]
 workouts.forEach {
-    let cbpm = $0.caloriesBurnedPerMinute
-    print("Calories burned per/min in \($0.name): \(cbpm)")
+    let s = String(repeating: "=", count: 50)
+    print(s)
+    print($0.title)
+    print("Calories burned per/min: \($0.caloriesBurnedPerMinute)")
+    print(s)
 }
 extension Sequence where Iterator.Element == Exercise {
     func totalCaloriesBurned() -> Double {
