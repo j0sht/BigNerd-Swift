ad40f292c9d83d84f14ca8189585b100862804cd
Joshua Tate <jmtate@icloud.com>
2017-05-01 20:55:15 -0600

    Added ch25/platinum solution

--- ch25/comparable.swift
+++ ch25/comparable.swift
@@ -8,8 +8,13 @@ extension Point: Comparable {
     static func ==(lhs: Point, rhs: Point) -> Bool {
         return (lhs.x == rhs.x) && (lhs.y == rhs.y)
     }
+    // Platinum challenge
+    // Return true for a < b when a is closer to origin than b
     static func <(lhs: Point, rhs: Point) -> Bool {
-        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
+        func sqr(_ x: Int) -> Int { return x * x }
+        let a = sqrt(Double(sqr(0 - lhs.x) + sqr(0 - lhs.y)))
+        let b = sqrt(Double(sqr(0 - rhs.x) + sqr(0 - rhs.y)))
+        return a < b
     }
 }
 extension Point: CustomStringConvertible {
@@ -27,3 +32,9 @@ extension Point {
 let a = Point(x: 1, y: 1)
 let b = Point(x: 2, y: 2)
 print("\(a) + \(b) = \(a + b)")
+// Platinum challenge output
+let c = Point(x: 3, y: 4)
+let d = Point(x: 2, y: 5)
+print("\(c) > \(d) = \(c > d)")
+print("\(c) < \(d) = \(c < d)")
+print("\(c) == \(d) = \(c == d)")
